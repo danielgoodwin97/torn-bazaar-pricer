eaafbd97b772818d085cc1eff8b7a0410eb73a73
Daniel Goodwin <epixz19@gmail.com>
2021-01-18 03:45:32 +0000

    Update version number for automatic updates
    Oopsy woopsy I made a stinky and didn't update the version number for the script meaning people aren't automatically getting update prompts in Tamper Monkey.

--- auto-bazaar-pricer.user.js
+++ auto-bazaar-pricer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn - Bazaar Pricer
 // @namespace    https://github.com/danielgoodwin97/torn-bazaar-pricer
-// @version      1.5.7
+// @version      1.5.8
 // @description  Automatically price & add quantity to bazaar items.
 // @author       FATU [1482556]
 // @match        *.torn.com/bazaar.php*
